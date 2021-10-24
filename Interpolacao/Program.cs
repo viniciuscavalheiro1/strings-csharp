f022a8d9666a643befd7ad1ea0ee9218154999e2
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-10-24 18:51:03 -0300

    feat: Atualizando README.md

---
+++ Interpolacao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Interpolacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var price = 10.9;
+            var price2 = 9.9;
+            var text = string.Format(
+              "O preço do produto é {0} e o preço na promoção {1}", 
+              price,
+              price2);
+            Console.WriteLine(text);
+            var text2 = $"O preço do produto é {price} e o preço na promoção {price2}";
+            Console.WriteLine(text2);
+        }
+    }
+}
