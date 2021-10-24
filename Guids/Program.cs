1eb67b74ddf95e14027a4b4528091d5a063b06be
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-10-24 16:05:45 -0300

    feat: Aprendendo Guids em C#

---
+++ Guids/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Guids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            Console.WriteLine(id.ToString().Substring(2, 8));
+        }
+    }
+}
